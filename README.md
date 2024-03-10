3e2e28b8ca37e5b8008a61c19515aab3ed1e896d
Yelko Veiga <yelkovq@gmail.com>
2024-03-10 21:54:06 +0100

    build(Cotxox): creating repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cotxox
+
+Solución al examen de programación Cotxox de **Yelko Veiga** (1º DAM IES de Teis 23/24) a propuesta de @dfleta.
+
+Tercer proyecto de aproximación a Java.
+
+## Examen programación Java
+
+
+
---
+++ app/src/main/java/edu/badpals/cotxox/Cotxox.java
@@ -0,0 +1,29 @@
+package edu.badpals.cotxox;
+
+public class Cotxox {
+
+    public static void main(String[] args) {
+
+        /*
+         * Configuracion del usuario
+         * y de la Carrera.
+         */
+
+        String tarjetaCredito = "4916119711304546";
+        String origen = "Aeroport Son Sant Joan";
+        String destino = "Magaluf";
+        double distancia = 7.75;
+        int tiempoEsperadoMinutos = 10;
+
+        Carrera carrera = new Carrera(tarjetaCredito);
+        carrera.setOrigen(origen);
+        carrera.setDestino(destino);
+        carrera.setDistancia(distancia);
+        carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+
+        System.out.println("\n#####" + "\t Set Pickup: \t" + "#####\n");
+
+        System.out.println("Visa: " + carrera.getTarjetaCredito());
+        System.out.println("From: " + carrera.getOrigen());
+        System.out.println("To: " + carrera.getDestino());
+        System.out.println("Distance: " + carrera.getDistancia());
